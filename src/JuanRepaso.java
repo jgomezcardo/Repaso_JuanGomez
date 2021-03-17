0d6d2d24636bb3c77643f08903867466f3cc464a
Ceinmark <Ceinmark@DESKTOP-J0PN6TT>
2021-03-17 12:02:29 +0100

    este es el programa original

---
+++ src/JuanRepaso.java
@@ -0,0 +1,15 @@
+
+public class JuanRepaso {
+
+	public static void main (String args[]) {
+	
+		System.out.println("programa de muestra haz lo que veas conveniente");
+		
+		for(int i=0; i<10;i++) {
+			System.out.println("esto es una modificacion");
+			
+		}
+		System.out.println("pim pan");
+	}
+	
+}
